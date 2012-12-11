edd12b423d2233a68a9512b0e80458d11be7df10
Steve.Schlotter <steve.schlotter@gmail.com>
2012-12-11 08:41:14 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biblereading2::Application.config.secret_token = '3a10692e12997bfe081c66dce15825e22ff970f52c3024df77661613e3634d79383e694b38c520ed2a945cf9e4fb6ce042ab534961810718963b1e1566350fbc'
